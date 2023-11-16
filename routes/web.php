c4839eac370d1e11ca151d089f85557e3efb0e5b
Melozzo <carmecamma1995@gmail.com>
2023-11-16 15:09:07 +0100

    [edit]: dinamic word

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,12 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('home');
+    $name = 'Melozzo';
+    $lastname = 'daCanicattì';
+
+    $data ={
+        name => $name;
+        lastname => $lastname;
+    }
+    return view('home', $date);
 });
